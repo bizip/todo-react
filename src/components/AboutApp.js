bfa0a5bcadf9d6dc2b35c1ee49db67176b1f4587
bizip <bizip04@gmail.com>
2022-06-10 08:27:04 +0200

    Add about details

--- src/components/AboutApp.js
+++ src/components/AboutApp.js
@@ -8,6 +8,13 @@ const AboutApp = () => (
   <div>
     {' '}
     <h1>About App</h1>
+    <p>
+      Prosciutto frankfurter bresaola, fatback filet mignon pastrami capicola.
+      Short loin sirloin chislic, pancetta alcatra cupim beef ribs chuck flank picanha
+      andouille fatback beef frankfurter. Boudin short loin kielbasa ball tip beef ribs
+      hamburger leberkas filet mignon
+      frankfurter short ribs kevin beef. Capicola ham beef ribs jerky shank.
+    </p>
   </div>
 );
 
--- src/components/AboutAuthor.js
+++ src/components/AboutAuthor.js
@@ -4,6 +4,13 @@ const AboutAuthor = () => (
   <div>
     {' '}
     <h1>About Author</h1>
+    <p>
+      Prosciutto frankfurter bresaola, fatback filet mignon pastrami capicola.
+      Short loin sirloin chislic, pancetta alcatra cupim beef ribs chuck flank picanha
+      andouille fatback beef frankfurter. Boudin short loin kielbasa ball tip beef ribs
+      hamburger leberkas filet mignon
+      frankfurter short ribs kevin beef. Capicola ham beef ribs jerky shank.
+    </p>
   </div>
 );
 
